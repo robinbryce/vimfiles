5c66336020dd7bba178b9f8014a35479e46f64af
Robin Bryce <robin.bryce@thales-esecurity.net>
2018-05-19 17:18:56 +0200

    Pre-vundle

---
+++ updateall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $scriptdir
+
+# Forced checkout, discarding any local changes
+git submodule update --recursive --remote --force
+# Now update to the latest of everything (equivelent of pull in each submodule)
+git submodule update --recursive --remote --merge
+git submodule update --recursive --init
+
+# Re-build
+echo "Re-building YouCompleteMe"
+cd $scriptdir/bundle/YouCompleteMe
+./install.py --clang-completer --go-completer
