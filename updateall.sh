b0fc3761b02a5864ae5900cb1bebd0b963509bba
Robin Bryce <robin.bryce@thales-esecurity.net>
2018-05-20 23:02:32 +0200

    Add command-t to re-build. It's a pita due to linking with ruby. The trick is to ensure it links against the brew install'd ruby

--- updateall.sh
+++ updateall.sh
@@ -13,3 +13,12 @@ cd $scriptdir
 echo "Re-building YouCompleteMe"
 cd $scriptdir/bundle/YouCompleteMe
 ./install.py --clang-completer --go-completer --js-completer --rust-completer
+
+echo "Re-buiding command-t"
+cd $scriptdir/bundle/command-t/ruby/command-t/ext/command-t
+# rbenv global 2.5.1 ?
+# This should pick up whatever rbenv has set (local or global) and will at
+# least be correct for command line vim.
+$(which ruby) extconf.rb
+make clean
+make
